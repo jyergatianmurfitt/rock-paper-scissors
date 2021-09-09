7c1d2b74f0e775c40e85397a12412065036f5433
Josephina <jyergatianmurfitt@googlemail.com>
2021-09-09 16:09:59 +0100

    added computer controls

--- script.js
+++ script.js
@@ -11,39 +11,62 @@ computerScoreBoard.textContent = 'Computer: ' + computerScore;
 
 
 const play = playerChoice => {
+  //Reset Computer controls//
+  const rockComp = document.querySelector('.rockComp');
+  const paperComp = document.querySelector('.paperComp');
+  const scissorsComp = document.querySelector('.scissorsComp');
+  const textResult = document.querySelector('.textResult');
+
+  rockComp.style.transform = 'scale(1)';
+  paperComp.style.transform = 'scale(1)';
+  scissorsComp.style.transform = 'scale(1)';
+
   //Computer's choice//
   const getRandomInt = max => {
   return Math.floor(Math.random() * max);
 }
   const choiceArr = ['rock', 'paper', 'scissors'];
-  const randomChoice = getRandomInt(2);
+  const randomChoice = getRandomInt(3);
   const computerChoice = choiceArr[randomChoice];
 
   //Check Choices//
   if (playerChoice === computerChoice) {
-      console.log('Draw! Play again.')
+      if (computerChoice === 'rock') {
+        rockComp.style.transform = 'scale(2)';
+      } else if (computerChoice === 'paper') {
+        paperComp.style.transform = 'scale(2)';
+      } else if (computerChoice === 'paper') {
+        scissorsComp.style.transform = 'scale(2)';
+      }
+      textResult.textContent = 'Draw! Play again';
     } else if (playerChoice === 'rock' ) {
       if (computerChoice === 'scissors') {
-        console.log('You won! Rock crushes scissors');
+        scissorsComp.style.transform = 'scale(2)';
+        textResult.textContent = 'You won! Rock crushes scissors';
         playerScore++;
       } else if (computerChoice === 'paper') {
-        console.log('You lose! Paper covers rock');
+        paperComp.style.transform = 'scale(2)';
+        textResult.textContent = 'You lose! Paper covers rock';
         computerScore++;
       }
     } else if (playerChoice === 'paper') {
       if (computerChoice === 'rock') {
-        console.log('You won! Paper covers rock');
+        rockComp.style.transform = 'scale(2)';
+        textResult.textContent = 'You won! Paper covers rock';
         playerScore++;
       } else if (computerChoice === 'scissors') {
-        console.log('You lose! Scissors cut paper');
+        scissorsComp.style.transform = 'scale(2)';
+        textResult.textContent = 'You lose! Scissors cut paper';
         computerScore++;
       }
     } else if (playerChoice === 'scissors') {
       if (computerChoice === 'rock') {
-        console.log('You lose! Rock crushes scissors');
+        rockComp.style.transform = 'scale(2)';
+        textResult.textContent = 'You lose! Rock crushes scissors';
         computerScore++;
       } else if (computerChoice === 'paper') {
-        console.log('You won! Scissors cut paper');
+        paperComp.style.transform = 'scale(2)';
+        textResult.textContent = 'You won! Scissors cut paper';
         playerScore++;
       }
     }
