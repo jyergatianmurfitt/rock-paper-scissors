08018ba9c181a368fd65a9540d0db4d78eab2ca0
Josephina <jyergatianmurfitt@googlemail.com>
2021-09-09 15:26:25 +0100

    scoreboard added

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
+//Display Score Board//
+let playerScore = 0;
+let computerScore = 0;
+
+const playerScoreBoard = document.querySelector('.playerScore');
+const computerScoreBoard = document.querySelector('.computerScore');
+
+playerScoreBoard.textContent = 'Player: ' + playerScore;
+computerScoreBoard.textContent = 'Computer: ' + computerScore;
+
+
+
 const play = playerChoice => {
   //Computer's choice//
   const getRandomInt = max => {
   return Math.floor(Math.random() * max);
 }
-
   const choiceArr = ['rock', 'paper', 'scissors'];
   const randomChoice = getRandomInt(2);
   const computerChoice = choiceArr[randomChoice];
 
-
   //Check Choices//
   if (playerChoice === computerChoice) {
-      alert('Draw! Play again.')
+      console.log('Draw! Play again.')
     } else if (playerChoice === 'rock' ) {
       if (computerChoice === 'scissors') {
-        alert('You won! Rock crushes scissors');
+        console.log('You won! Rock crushes scissors');
+        playerScore++;
       } else if (computerChoice === 'paper') {
-        alert('You lose! Paper covers rock');
+        console.log('You lose! Paper covers rock');
+        computerScore++;
       }
     } else if (playerChoice === 'paper') {
       if (computerChoice === 'rock') {
-        alert('You won! Paper covers rock');
+        console.log('You won! Paper covers rock');
+        playerScore++;
       } else if (computerChoice === 'scissors') {
-        alert('You lose! Scissors cut paper');
+        console.log('You lose! Scissors cut paper');
+        computerScore++;
       }
     } else if (playerChoice === 'scissors') {
       if (computerChoice === 'rock') {
-        alert('You lose! Rock crushes scissors');
+        console.log('You lose! Rock crushes scissors');
+        computerScore++;
       } else if (computerChoice === 'paper') {
-        alert('You won! Scissors cut paper');
+        console.log('You won! Scissors cut paper');
+        playerScore++;
       }
     }
 
+    //Update Score Board//
+    playerScoreBoard.textContent = 'Player: ' + playerScore;
+    computerScoreBoard.textContent = 'Computer: ' + computerScore;
 
 
 }
